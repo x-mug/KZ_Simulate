f7a3790fc443234e77b666164338e310f0b91d19
x-mug <80018905+x-mug@users.noreply.github.com>
2021-03-14 21:06:57 +0800

    Initial commit

---
+++ Source/KZ_Simulate/KZ_Simulate.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KZ_Simulate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KZ_Simulate, "KZ_Simulate" );
